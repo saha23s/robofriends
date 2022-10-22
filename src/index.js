e48a0bce0a4baf8b0c6942c0fd2b765795d62b61
Sulagna Saha <saha23s@mtholyoke.edu>
2022-10-22 11:19:40 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import {robots} from './Robot';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <App />
+    </div>
   </React.StrictMode>
 );
 
